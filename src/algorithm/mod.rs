a865e4f26bbb8cb2446968c6c76871b54568eb59
Felix-Droop <felix.droop@fu-berlin.de>
2025-05-13 11:44:54 +0200

    refactor: move inducing functions into own module

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,6 +1,8 @@
 // #[cfg(test)]
 // mod tests;
 
+mod inducing;
+
 use crate::Character;
 
 use bitvec::prelude::*;
@@ -62,7 +64,7 @@ pub fn suffix_array_induced_sort<C: Character>(
         text,
     );
 
-    induce_to_sort_lms_substrings(
+    inducing::induce_to_sort_lms_substrings(
         suffix_array_buffer,
         bucket_start_indices,
         bucket_indices_working_buffer,
@@ -116,7 +118,7 @@ pub fn suffix_array_induced_sort<C: Character>(
         text,
     );
 
-    induce_to_finalize_suffix_array(
+    inducing::induce_to_finalize_suffix_array(
         suffix_array_buffer,
         bucket_start_indices,
         bucket_indices_working_buffer,
@@ -219,140 +221,6 @@ fn place_sorted_lms_indices_into_buckets<C: Character>(
         suffix_array_buffer[bucket_start_index..=bucket_end_index_after_lms_placement]
             .fill(NONE_VALUE);
     }
-}
-
-// after this, the sorted LMS indices (by LMS substrings) are at the end of suffix_array_buffer
-fn induce_to_sort_lms_substrings<C: Character>(
-    suffix_array_buffer: &mut [usize],
-    bucket_start_indices: &[usize],
-    bucket_indices_buffer: &mut [usize],
-    is_s_type: &BitSlice,
-    text: &[C],
-) {
-    // ---------- LEFT TO RIGHT SCAN ----------
-    bucket_indices_buffer.copy_from_slice(bucket_start_indices);
-
-    // virtual sentinel induction, it would normally be at first position of the suffix array
-    let last_suffix_index = text.len() - 1;
-    let last_suffix_char = text[last_suffix_index];
-    let last_suffix_bucket_start_index = &mut bucket_indices_buffer[last_suffix_char.rank()];
-    suffix_array_buffer[*last_suffix_bucket_start_index] = last_suffix_index;
-    *last_suffix_bucket_start_index += 1;
-
-    for suffix_array_index in 0..suffix_array_buffer.len() {
-        let suffix_index = suffix_array_buffer[suffix_array_index];
-
-        if suffix_index == NONE_VALUE || suffix_index == 0 || is_s_type[suffix_index - 1] {
-            continue;
-        }
-
-        let induced_suffix_first_char = text[suffix_index - 1];
-        let induced_suffix_bucket_start_index =
-            &mut bucket_indices_buffer[induced_suffix_first_char.rank()];
-
-        suffix_array_buffer[*induced_suffix_bucket_start_index] = suffix_index - 1;
-        *induced_suffix_bucket_start_index += 1;
-    }
-
-    // ---------- RIGHT TO LEFT SCAN ----------
-    write_bucket_end_indices_into_buffer(bucket_start_indices, bucket_indices_buffer, text.len());
-
-    let mut write_index = suffix_array_buffer.len() - 1;
-
-    for suffix_array_read_index in (0..suffix_array_buffer.len()).rev() {
-        let suffix_index = suffix_array_buffer[suffix_array_read_index];
-
-        // no need to check for EMPTY_VALUE here, because in this iteration, every index of the suffix
-        // array buffer must have been written to before (L-type in previous scan, S-type in this one)
-        if suffix_index == 0 {
-            continue;
-        }
-
-        // the LMS suffixes only induce L-type suffixes, which we are not interested in
-        // instead, we prepare for creation of reduced text by moving all of the
-        // LMS indices to the back of the array (now sorted by LMS substrings)
-        if is_lms_type(suffix_index, is_s_type) {
-            suffix_array_buffer[write_index] = suffix_index;
-            write_index -= 1;
-            continue;
-        }
-
-        if !is_s_type[suffix_index - 1] {
-            continue;
-        }
-
-        let induced_suffix_first_char = text[suffix_index - 1];
-        let induced_suffix_bucket_end_index =
-            &mut bucket_indices_buffer[induced_suffix_first_char.rank()];
-
-        suffix_array_buffer[*induced_suffix_bucket_end_index] = suffix_index - 1;
-
-        // saturating sub used, because the last placement of the first bucket (index 0) otherwise might underflow
-        // (it is okay to keep zero, because it is never read again. might also just use underflowing function)
-        *induced_suffix_bucket_end_index = induced_suffix_bucket_end_index.saturating_sub(1);
-    }
-
-    // on the right to left scan, the sentinel does not induce anything,
-    // because the char before it is always L-type
-}
-
-fn induce_to_finalize_suffix_array<C: Character>(
-    suffix_array_buffer: &mut [usize],
-    bucket_start_indices: &[usize],
-    bucket_indices_buffer: &mut [usize],
-    is_s_type: &BitSlice,
-    text: &[C],
-) {
-    // ---------- LEFT TO RIGHT SCAN ----------
-    bucket_indices_buffer.copy_from_slice(bucket_start_indices);
-
-    // virtual sentinel induction, it would normally be at first position of the suffix array
-    let last_suffix_index = text.len() - 1;
-    let last_suffix_char = text[last_suffix_index];
-    let last_suffix_bucket_start_index = &mut bucket_indices_buffer[last_suffix_char.rank()];
-    suffix_array_buffer[*last_suffix_bucket_start_index] = last_suffix_index;
-    *last_suffix_bucket_start_index += 1;
-
-    for suffix_array_index in 0..suffix_array_buffer.len() {
-        let suffix_index = suffix_array_buffer[suffix_array_index];
-
-        if suffix_index == NONE_VALUE || suffix_index == 0 || is_s_type[suffix_index - 1] {
-            continue;
-        }
-
-        let induced_suffix_first_char = text[suffix_index - 1];
-        let induced_suffix_bucket_start_index =
-            &mut bucket_indices_buffer[induced_suffix_first_char.rank()];
-
-        suffix_array_buffer[*induced_suffix_bucket_start_index] = suffix_index - 1;
-        *induced_suffix_bucket_start_index += 1;
-    }
-
-    // ---------- RIGHT TO LEFT SCAN ----------
-    write_bucket_end_indices_into_buffer(bucket_start_indices, bucket_indices_buffer, text.len());
-
-    for suffix_array_index in (0..suffix_array_buffer.len()).rev() {
-        let suffix_index = suffix_array_buffer[suffix_array_index];
-
-        // no need to check for EMPTY_VALUE here, because in this iteration, every index of the suffix
-        // array buffer must have been written to before (L-type in previous scan, S-type in this one)
-        if suffix_index == 0 || !is_s_type[suffix_index - 1] {
-            continue;
-        }
-
-        let induced_suffix_first_char = text[suffix_index - 1];
-        let induced_suffix_bucket_end_index =
-            &mut bucket_indices_buffer[induced_suffix_first_char.rank()];
-
-        suffix_array_buffer[*induced_suffix_bucket_end_index] = suffix_index - 1;
-
-        // saturating sub used, because the last placement of the first bucket (index 0) otherwise might underflow
-        // (it is okay to keep zero, because it is never read again. might also just use underflowing function)
-        *induced_suffix_bucket_end_index = induced_suffix_bucket_end_index.saturating_sub(1);
-    }
-
-    // on the right to left scan, the sentinel does not induce anything,
-    // because the char before it is always L-type
 }
 
 // reduced text is written to the end of the suffix array buffer
